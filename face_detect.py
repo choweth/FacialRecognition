0e31bf9f00cfa0e74f788b44fb0db183698f2ad2
Clayton Howeth <choweth@mymail.mines.edu>
2016-05-19 13:12:24 -0600

    Trying to merge the face_detect.py file

--- face_detect.py
+++ face_detect.py
@@ -4,7 +4,7 @@ import math
 import time
 import numpy
 
-def findFaces(imagePath):
+def findFaces(image):
 # Get user supplied values
     cascPath = "haarcascade_frontalface_default.xml"
 
@@ -13,7 +13,6 @@ def findFaces(imagePath):
     faceCascade = cv2.CascadeClassifier(cascPath)
 
     # Read the image
-    image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
@@ -36,6 +35,9 @@ def findFaces(imagePath):
     #cv2.imshow("Faces found", image)
     #cv2.imwrite("Output.jpg", image)
     #cv2.waitKey(0)
+
+    
+
     return faces
 
 def cropScaleImage(img, x, y, w, h):
@@ -52,53 +54,69 @@ def cropScaleImage(img, x, y, w, h):
 
     return newerImage
 
-def imageToVector(img):
-    l = []
-    for i in range(0, 599):
-        for j in range(0, 499):
-            l.append(img[i,j])
+def rotateImage(img, angle):
+    rows, cols = img.shape[:2]
+    M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
+    rotatedImage = cv2.warpAffine(img, M, (cols,rows))
+    return rotatedImage
 
-def averageFaces(faces):
-    newPic = numpy.empty((600,500,3), int)
-    avgVal = 0
-    print len(newPic[0,0])
-    for i in range(600):
-        for j in range(500):
-            for l in range(len(faces)):
-                avgVal = avgVal + faces[l][i,j][0]
-            x = int((avgVal / len(faces)))
-            for k in range(3):
-                newPic[i,j,k] = x
-            avgVal = 0
-    return newPic
+def compressImage(img, bound):
+    rows, cols = img.shape[:2]
+    rows = int(rows)
+    cols = int(cols)
+
+    if (rows > bound and rows >= cols):
+        newRow = bound
+        newCol = int(cols*(bound/float(rows)))
+        img = cv2.resize(img, (newCol, newRow), interpolation = cv2.INTER_AREA)
+
+    elif (cols > bound and rows < cols):
+        newCol = bound
+        newRow = int(rows*(bound/float(cols)))
+        img = cv2.resize(img, (newCol, newRow), interpolation = cv2.INTER_AREA)
+
+    return img
 
 if __name__ =="__main__":
-    pic = "Images/abba.png"
-
-    faces = findFaces(pic)
+    pic = "Images/a.jpg"
     image = cv2.imread(pic)
+    #Compresses the picture down so the longest side is 1200 pixels. Keeps aspect ratio
+    image = compressImage(image, 1280)
+    faces = findFaces(image)
+    global rotatedImage
+    
+    # Rotates the image 30 degrees if no faces found
+    if (len(faces) == 0):
+        print "Rotating 30 counter clockwise..."
+        rotatedImage = rotateImage(image, 30)
+        faces = findFaces(rotatedImage)
+        if (len(faces) != 0): image = rotatedImage
+
+    # Rotates 30 degrees in the opposite direction
+    if (len(faces) == 0):
+        print "Rotating 30 clockwise..."
+        rotatedImage = rotateImage(image, -30)
+        faces = findFaces(rotatedImage)
+        if (len(faces) != 0): image = rotatedImage
 
     i=0
-    newFaces = [None] * len(faces)
+    croppedFaces = [None] * len(faces)
 
+    # Crops out and scales each found face
     for (x, y, w, h) in faces:
-        newFaces[i] = cropScaleImage(image, x, y, w, h)
+        croppedFaces[i] = cropScaleImage(image, x, y, w, h)
         i += 1
 
-    meanFace = averageFaces(newFaces)
-    print meanFace
-    cv2.imshow("Mean Face", meanFace)
-    cv2.imwrite("Output/mf_Output.jpg", meanFace)
-
+    # Draws a rectangle around each found face
     for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y-int(h*0.1)), (x+w, int(y+h*1.1)), (0, 255, 0), 2)
 
+    # Writes each cropped face to its own file
     i = 0
-    for img in newFaces:
+    for img in croppedFaces:
         cv2.imwrite("Output/Output_" + str(i) +  ".jpg", img)
         i += 1
 
-    # print image
     print "Found {0} faces!".format(len(faces))
     cv2.imshow("Faces found", image)
     cv2.imwrite("Output/Output.jpg", image)
