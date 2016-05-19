700018ad3a5244331073269ea5e9690643c082bf
Jed M <jmenard@mymail.mines.edu>
2016-05-19 10:10:01 -0600

    Added code to crop and resize faces

--- face_detect.py
+++ face_detect.py
@@ -1,15 +1,13 @@
 import cv2
 import sys
+import math
 import time
+import numpy
 
 def findFaces(imagePath):
 # Get user supplied values
-<<<<<<< HEAD
     cascPath = "haarcascade_frontalface_default.xml"
-=======
-imagePath = "b.png"
-cascPath = "haarcascade_frontalface_default.xml"
->>>>>>> d0ca85bb8aa0557c054f31146fa34f61a138cacb
+
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
@@ -18,7 +16,7 @@ cascPath = "haarcascade_frontalface_default.xml"
     image = cv2.imread(imagePath)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-<<<<<<< HEAD
+
     # Detect faces in the image
     #now = time.time()
     faces = faceCascade.detectMultiScale(
@@ -30,34 +28,51 @@ cascPath = "haarcascade_frontalface_default.xml"
     )
     #print time.time() - now
     #print "Found {0} faces!".format(len(faces))
-=======
-# Detect faces in the image
-now = time.time()
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=10,
-    minSize=(50, 50), #size of crop region
-    flags = 0
-)
-print time.time() - now
-print "Found {0} faces!".format(len(faces))
->>>>>>> d0ca85bb8aa0557c054f31146fa34f61a138cacb
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    #for (x, y, w, h) in faces:
+    #    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     #cv2.imshow("Faces found", image)
     #cv2.imwrite("Output.jpg", image)
     #cv2.waitKey(0)
     return faces
+
+def cropScaleImage(img, x, y, w, h):
+    newImage = img[y:y+h, x:x+w]
+
+    height = 600
+    width = 500
+
+    if (h > height):
+        newerImage = cv2.resize(newImage, (width, height), interpolation = cv2.INTER_AREA)
+
+    else:
+        newerImage = cv2.resize(newImage,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+
+    return newerImage
+
 if __name__ =="__main__":
     faces = findFaces("Images/abba.png")
     image = cv2.imread("Images/abba.png")
+
+    i=0
+    newFaces = [None] * len(faces)
+
     for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        newFaces[i] = cropScaleImage(image, x, y, w, h)
+        i += 1
+
+    for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y-int(h*0.1)), (x+w, int(y+h*1.1)), (0, 255, 0), 2)
+
+    i = 0
+    for img in newFaces:
+        cv2.imshow("Found face", img)
+        cv2.imwrite("Output_" + str(i) +  ".jpg", img)
+        i += 1
+
     print "Found {0} faces!".format(len(faces))
-    cv2.imshow("Faces found", image)
+    #cv2.imshow("Faces found", image)
     cv2.imwrite("Output.jpg", image)
     cv2.waitKey(0)
