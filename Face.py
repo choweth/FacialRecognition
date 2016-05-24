a601e0ac4cd095b7271fc5b88f3d27d4e1baf3ef
Jed M <jmenard@mymail.mines.edu>
2016-05-24 08:46:47 -0600

    Commented Face.py, renamed 'Support files'

--- Face.py
+++ Face.py
@@ -12,23 +12,31 @@ class Face:
         self.orig = iManip.cropScaleImage(image,x,y,w,h)    #The original image
         self.grayFace = iManip.grayFace(self.orig)          #The grayscale face
     
+    # Compares the calling face with the given face.
+    # Returns the percent similarity
     def compare(compFace):
         epsSquared = abs(faceSpaceProj - compFace.faceSpaceProj) ^ 2
         return 1 - epsSquared
 
+    # Initializes the DifferenceFace and differenceVector
     def initDiff(self, meanFace):
         self.diffFace = iManip.differenceFace(self.grayFace, meanFace)
         self.diffVec = iManip.imageToVector(self.diffFace)
-        
+    
+    # Initializes the eigenFace and eigenVector
     def initEigenFace(self, eVec):
         self.eigenVec = eVec
         self.eigenFace = iManip.vectorToImage(iManip.scaleVals(eVec))
 
+    # Projects the original image onto the faceSpace
+    # This is used to compare an eigenface with the original face
+    # This can be used to assure the picture even is a face to begin with
     def initProjections(self, faceSpace):
         self.faceSpaceProj = numpy.matmul(faceSpace,self.diffVec)
+        grayProj = numpy.matmul(numpy.matmul(faceSpace,faceSpace.transpose), grayFace)
         print faceSpaceProj.shape    
 
+    # Returns the percent probability that a face image actually contains a face
     def isFace(faceSpace):
-        grayProj = numpy.matmul(numpy.matmul(faceSpace,faceSpace.transpose), grayFace)
         epsSquared = abs(grayFace - grayProj) ^ 2
         return 1 - epsSquared
