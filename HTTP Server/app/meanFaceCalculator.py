16f5dc6f73fb276d098be43b850936b3f6bb69a3
Jed M <jmenard@mymail.mines.edu>
2016-06-01 13:04:16 -0600

    Implemented face detection and mean face calculator in the HTTP server

--- HTTP Server/app/meanFaceCalculator.py
+++ HTTP Server/app/meanFaceCalculator.py
@@ -1,4 +1,8 @@
 from app import app
+from flask import request
+from src import ImgManipulation as iManip
+import cv2
+import numpy as np
 
 @app.route('/meanFaceCalculator', methods = ['GET'])
 def atMeanFaceCalc():
@@ -6,5 +10,17 @@ def atMeanFaceCalc():
 
 @app.route('/meanFaceCalculator', methods = ['POST'])
 def postMeanFaceCalc():
-	# Calculate the mean face
-	return "This page will calculate the mean face"
+    # Read the faces from the files sent
+    faces = []
+    i =0
+    for key in request.files:
+        buf = request.files[key].read()
+        x = np.fromstring(buf, dtype = 'uint8')
+        faces[i] = cv2.imdecode(x, cv2.IMREAD_UNCHANGED)
+        i += 1
+
+    meanFace = iManip.averageFaces(faces)
+
+    flag, buf = cv2.imencode("return.jpg", meanFace, [cv2.IMWRITE_JPEG_QUALITY, 90])
+
+    return buf.tobytes()
