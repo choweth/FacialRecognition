01b60e36746c185dc5f3c16d5704d0e3127269a6
Jed M <jmenard@mymail.mines.edu>
2016-06-08 21:51:00 -0600

    Edited the black box endpoint so it doesn't have to communicate with others

--- HTTP Server/app/WhoAmI.py
+++ HTTP Server/app/WhoAmI.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import request
 import requests
+import Encryption
+import Extractor
 
 @app.route('/WhoAmI', methods = ['GET'])
 def atWhoAmI():
@@ -8,14 +10,20 @@ def atWhoAmI():
 
 @app.route('/WhoAmI', methods = ['POST'])
 def whoAmI():
-    r = requests.post('http://localhost/FaceExtractor', files = request.files)
-    faces = r.content['faces']
+    image = Encryption.decode(request.files['image'])
+    #r = requests.post('http://localhost/FaceExtractor', files = request.files)
+    faces = Extractor.extractFaces(image, Extractor.detectFaces(image))
 
     ids = [0 for i in range(len(faces))]
     i = 0
     for face in faces:
-        r = requests.post('http://localhost/FaceProcessor', files = {'image': face})
-        ids[i] = r.content
+        #r = requests.post('http://localhost/FaceProcessor', files = {'image': face})
+        grayFace = iManip.grayFace(image)
+
+        diffFace = CalcDiffFace.calc(grayFace)
+        diffFace = iManip.imageToVector(diffFace)
+
+        ids[i] = Database.Database.storeFace(grayFace, image, diffFace)
         i += 1
         
     return 'gj'
