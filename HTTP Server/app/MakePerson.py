e21421f37be928a6d48530c22e1c0bae2fa1399a
Jed M <jmenard@mymail.mines.edu>
2016-06-08 20:58:04 -0600

    Updated some Database code

--- HTTP Server/app/MakePerson.py
+++ HTTP Server/app/MakePerson.py
@@ -8,22 +8,17 @@ import requests
 import Person
 import Database
 
-@app.route('MakePerson', methods = ['GET'])
+@app.route('/MakePerson', methods = ['GET'])
 def atMakePerson():
     return "This page makes a Person object"
 
-@app.route('makeperson', methods = ['post'])
-def atmakeperson():
+@app.route('/MakePerson', methods = ['POST'])
+def makePerson():
     
     imgs = request.form['imgs']
-    #raw_images = [0 for i in range(len(imgs))]
-    #i = 0
-    #for img in imgs:
-    #    raw_images[i] = cv2.imread(os.getcwd+"/Data/raw_images/IMG_" + str(img) + ".jpg")
-    #    i += 1
     faces = []
     for image in imgs:
-        r = requests.post('https://localhost/FaceExtractor', files = {'image': open(os.getcwd+"/Data/raw_images/IMG_" + str(img) + ".jpg",'rb').read()})
+        r = requests.post('https://localhost/FaceExtractor', files = {'image': open(os.getcwd()+"/Data/raw_images/IMG_" + str(image) + ".jpg",'rb').read()})
         faces.append(r.content['faces'])
 
     ids = [0 for i in range(len(faces))]
